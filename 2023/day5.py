b9f99c69c62e4a5362a5f9d8fd096176d6472463
skarre-r <59067313+skarre-r@users.noreply.github.com>
2023-12-05 20:41:49 +0100

    chore: start working on the day 5 puzzle

--- 2023/day5.py
+++ 2023/day5.py
@@ -1,5 +1,34 @@
-def main() -> None:
+"""
+TODO.
+"""
+
+from __future__ import annotations
+
+
+def process_input() -> ...:
+    with open(file="day5.txt", mode="r", newline="\n") as input_file:
+        lines: list[str] = [l for line in input_file.readlines() if (l := line.strip())]
+    seeds: list[int] = [int(seed) for seed in (lines[0].split(":")[1]).split(" ") if seed]
+
+    maps: list[...] = []
+
+    return seeds, maps
+
+
+def solve_part_1(seeds: list[int], maps: list[...]) -> ...:
     ...
+
+
+def solve_part_2(seeds: list[int], maps: list[...]) -> ...:
+    ...
+
+
+def main() -> None:
+    seeds, maps = process_input()
+    part_1_answer = solve_part_1(seeds, maps)
+    print(f"{part_1_answer=}")
+    part_2_answer = solve_part_2(seeds, maps)
+    print(f"{part_2_answer=}")
 
 
 if __name__ == "__main__":
